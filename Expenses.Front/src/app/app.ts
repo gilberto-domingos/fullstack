7d399b826f01c2eaabd10317489d4ddb39bdc011
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-25 22:59:30 -0300

    feat: adiciona rotas (#11)

--- Expenses.Front/src/app/app.ts
+++ Expenses.Front/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './footer/footer';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Expenses.Front');
