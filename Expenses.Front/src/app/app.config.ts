ae4f2fa7b37494b32d93adb1168876e54448a56e
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-07-27 04:10:19 -0300

    feat: adiciona tema estilo (#14)

--- Expenses.Front/src/app/app.config.ts
+++ Expenses.Front/src/app/app.config.ts
@@ -1,13 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
+providers: [importProvidersFrom()];
+
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimations(),
+
+    importProvidersFrom(),
+  ],
 };
--- Expenses.Front/src/app/components/header/header.ts
+++ Expenses.Front/src/app/components/header/header.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatToolbarModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  onAddTransaction() {
+    console.log('Add Transaction clicked');
+    // lógica para abrir form ou navegar
+  }
 
+  onLogout() {
+    console.log('Logout clicked');
+    // lógica de logout
+  }
+
+  onLogin() {
+    console.log('Login clicked');
+    // lógica de login
+  }
 }
