013c9cd04cc7eaa8a059812b91b393098bf9378f
gilberto-domingos <gilberto1domingos1@outlook.com>
2025-08-21 19:24:43 -0300

    chore: config (#35)

--- Expenses.API/Expenses.API/Data/AppDbContext.cs
+++ Expenses.API/Expenses.API/Data/AppDbContext.cs
@@ -9,7 +9,39 @@ namespace Expenses.API.Data
         {
         }
 
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<PrintJob> PrintJobs { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Transaction> Transactions { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PrintJob>(entity =>
+            {
+                entity.HasKey(p => p.PrintJobId); 
+
+                entity.HasOne(p => p.Student)
+                    .WithMany(s => s.PrintJobs)   
+                    .HasForeignKey(p => p.StudentId)
+                    .OnDelete(DeleteBehavior.Cascade);
+                
+                
+            });
+            
+            modelBuilder.Entity<Purchase>(entity =>
+            {
+                entity.HasKey(p => p.PurchaseId);
+
+                entity.HasOne(p => p.Student)
+                    .WithMany(s => s.Purchases)
+                    .HasForeignKey(p => p.StudentId)
+                    .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
     }
+    
 }
